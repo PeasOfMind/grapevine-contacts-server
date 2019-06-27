87399f8df5168db8d505f6bf59663fb312d44e7b
Yeqing Liu <40677349+PeasOfMind@users.noreply.github.com>
2019-06-26 20:20:12 -0400

    Update routes and formatting

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const morgan = require("morgan");
 mongoose.Promise = global.Promise;
 
 const { PORT, DATABASE_URL } = require("./config");
+const { router } = require("./contacts/router");
 
 const app = express();
 
@@ -23,40 +24,32 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use("/api", router);
+app.use("*", (req, res) => {
+  return res.status(404).json({ message: "Not Found" });
+});
+
 let server;
 
-function runServer(databaseUrl, port = PORT){
-    return new Promise((resolve, reject) => {
-        mongoose.connect(databaseUrl, err => {
-            if(err){
-                return reject(err);
-            }
-            server = app.listen(port, () => {
-                console.log(`Your app is listening on port ${port}`);
-                resolve();
-            })
-            .on('error', err => {
-                mongoose.disconnect();
-                reject(err);
-            });
+function runServer(databaseUrl, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app
+        .listen(port, () => {
+          console.log(`Your app is listening on port ${port}`);
+          resolve();
+        })
+        .on("error", err => {
+          mongoose.disconnect();
+          reject(err);
         });
     });
+  });
 }
 
-function closeServer(){
-    return mongoose.disconnect().then(() => {
-        return new Promise((resolve, reject) => {
-            console.log('Closing server');
-            server.close(err => {
-                if(err) {
-                    return reject(err);
-                }
-                resolve();
-            })
-        })
-    })
-}
-
-if (require.main === module){
-    runServer(DATABASE_URL).catch(err => console.error(err));
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
 }
