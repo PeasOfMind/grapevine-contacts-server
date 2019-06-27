8663048e784b594f638850b3debfb39803df988f
Yeqing Liu <40677349+PeasOfMind@users.noreply.github.com>
2019-06-26 20:19:27 -0400

    Add config variables for port and database url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 "use strict";
 
 exports.DATABASE_URL =
-  process.env.DATABASE_URL || "mongodb://localhost/item-adoption-api";
+  process.env.DATABASE_URL || "mongodb://localhost/grapevine-contacts-server";
 exports.PORT = process.env.PORT || 8080;
